a0098085780d7ee9e51aee776a0903baf1b6abb9
MS_CHA <getup0269@gmail.com>
2024-09-11 01:11:27 +0900

    Delete 장고 심화 개인과제/spartamarket_DRF directory

--- 장고 심화 개인과제/spartamarket_DRF/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-
-class UserCreationForm(forms.Form):
-    username = forms.CharField(max_length=10)
-    password = forms.CharField()
-    email = forms.EmailField()
--- 장고 심화 개인과제/spartamarket_DRF/products/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import home, login, profile, signup
-
-urlpatterns = [
-    parh("", ProductListView.as_view()),
-]
